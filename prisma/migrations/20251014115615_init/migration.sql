9d796601a19f30ce76efd6ad313964163e448f64
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-10-14 17:45:05 +0530

    Add Transaction Model in Prisma

---
+++ prisma/migrations/20251014115615_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+
